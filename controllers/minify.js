9af53ac10506dc6e8dd581e6e8c8a2452389076d
Paweł Wilk <wilkuwdr2008@gmail.com>
2021-10-06 13:27:25 +0200

    dependencies loaded

--- controllers/minify.js
+++ controllers/minify.js
@@ -1,5 +1,7 @@
 /* Singleton Pattern */
 const app = require('../app');
+const minify = require('@node-minify/core');
+const cssnano = require('@node-minify/cssnano');
 
 class minify {
     constructor(){
